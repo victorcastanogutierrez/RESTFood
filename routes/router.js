ea60555c21256dcd874a96205b22d998752226d2
Victor Castaño <victorcastano@MacBook-Pro-de-Victor.local>
2017-10-26 22:48:05 +0200

    Home inicial, router de paginas privadas

---
+++ routes/router.js
@@ -0,0 +1,16 @@
+module.exports = function(app, express, swig) {
+
+    var routerUsuarioSession = express.Router(); 
+    routerUsuarioSession.use(function(req, res, next) {
+        if (req.baseUrl && req.baseUrl.startsWith("/p/")) {
+            if (!req.session.usuario) {
+                res.redirect("/acceso");
+            }
+        } else {
+            next();
+        }
+    });
+    
+    //Aplicar routerUsuarioSession
+    app.use("*",routerUsuarioSession);
+}
--- routes/rrestaurantes.js
+++ routes/rrestaurantes.js
@@ -5,4 +5,16 @@ module.exports = function(app, swig, gestorDBUsuarios) {
         res.send(respuesta);
     });
 
+    app.get("/home", function(req, res) {
+        let restaurantes = [];
+        for (let i = 0; i < 10; i++) {
+            restaurantes.push({
+                nombre: 'Prueba nombre ' + i,
+                direccion: 'Prueba dirección' + i
+            });
+        }
+        var respuesta = swig.renderFile('views/vista_home.html', {restaurantes: restaurantes});
+        res.send(respuesta);
+    });
+
 }
