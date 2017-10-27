046d5d353be5945bdd60556e8a1f56d96fdb7dd4
Victor Castaño <victorcastano@MacBook-Pro-de-Victor.local>
2017-10-27 18:21:14 +0200

    Solución bug router

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,8 @@ module.exports = function(app, express, swig) {
 
     var routerUsuarioSession = express.Router();
     routerUsuarioSession.use(function(req, res, next) {
-        if (req.baseUrl && req.baseUrl.startsWith("/p/")) {
-            if (!req.session.usuario) {
-                res.redirect("/acceso");
-            }
+        if (req.baseUrl && req.baseUrl.startsWith("/p/") && !req.session.usuario) {
+            res.redirect("/acceso");
         } else {
             next();
         }
