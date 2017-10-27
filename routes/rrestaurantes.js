35d236892e6ff5069793cc9b51cb3c1383e407f3
Ignacio Fernández <nachoveiga1014@gmail.com>
2017-10-27 18:58:17 +0200

    checks en crear

--- routes/rrestaurantes.js
+++ routes/rrestaurantes.js
@@ -6,26 +6,25 @@ module.exports = function(app, swig, gestorDBUsuarios, restauranteGestorDB) {
     });
 
 
-    app.post("/restaurante", function(req, res) {
+    app.post("/p/restaurante", function(req, res) {
         let restaurante = req.body;
-        console.log(restaurante);
         restauranteGestorDB.insertarRestaurante(restaurante, (id) => {
             res.redirect("/views/vista_home.html");
         })
     });
 
-    app.get("/crearrestaurante", function(req, res) {
+    app.get("/p/crearrestaurante", function(req, res) {
         var respuesta = swig.renderFile('views/crear_restaurante.html', {});
         res.send(respuesta);
     });
 
     app.get("/home", function(req, res) {
         let restaurantes = [];
-        
+
         restauranteGestorDB.buscarRestaurantes(result => {
             var respuesta = swig.renderFile('views/vista_home.html', { restaurantes: result });
             res.send(respuesta);
-        });        
+        });
     });
 
 }
